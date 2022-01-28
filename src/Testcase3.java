6875fe6dd49ea7ec2d4b816e2ff4df5316a9d56c
NagarjunShanker <nagarjun9216@gmail.com>
2022-01-28 15:28:01 +0530

    TestCase 4 added to new branch

---
+++ src/Testcase3.java
@@ -0,0 +1,8 @@
+
+public class Testcase3 {
+
+	public static void main(String[] args) {
+		System.out.println("Keep this class in new branch");
+	}
+
+}
---
+++ src/batch/Testcase4.java
@@ -0,0 +1,10 @@
+package batch;
+
+public class Testcase4 {
+
+	public static void main(String[] args) {
+		System.out.println("Keep testcase4 in new batch");
+
+	}
+
+}
